5d453769c52d947a67d82dda4276400479594e61
Aimeos <aimeos@aimeos.org>
2021-05-14 17:52:37 +0200

    Assets for Github pages

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# upscheme
+<a class="badge" href="https://github.com/aimeos/upscheme/actions"><img src="https://github.com/aimeos/upscheme/actions/workflows/ci.yml/badge.svg" alt="Build Status" height="20"></a>
+<a class="badge" href="https://coveralls.io/github/aimeos/upscheme"><img src="https://coveralls.io/repos/github/aimeos/upscheme/badge.svg" alt="Coverage Status" height="20"></a>
+<a class="badge" href="https://packagist.org/packages/aimeos/upscheme"><img src="https://poser.pugx.org/aimeos/upscheme/license.svg" alt="License" height="20"></a>
+
+# Upscheme: Database schema updates made easy
+
+Easy to use PHP package for updating the database schema of your application
+and migrate data between versions.
+
+```bash
+composer req aimeos/upscheme
+```
+
+**Table of contents**
+
+* [Why Upscheme](#why-upscheme)
+
+## Why Upscheme
---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+(function() {
+	const nav = document.querySelector('main nav');
+	const methods = document.querySelector('.sidebar .methods');
+
+	if(nav && methods) {
+		methods.appendChild(nav.cloneNode(true));
+	}
+
+	const open = document.querySelector('.open');
+	const close = document.querySelector('.close');
+	const sidebar = document.querySelector('.sidebar');
+	const content = document.querySelector('.main-content');
+
+	open.addEventListener('click', function() {
+		open.classList.toggle('show');
+		close.classList.toggle('show');
+		sidebar.classList.toggle('show');
+	});
+
+	close.addEventListener('click', function() {
+		sidebar.classList.toggle('show');
+		close.classList.toggle('show');
+		open.classList.toggle('show');
+	});
+
+	const fcn = function() {
+		sidebar.classList.remove('show');
+		content.classList.remove('show');
+		close.classList.remove('show');
+		open.classList.add('show');
+	};
+
+	methods.addEventListener('click', fcn);
+	content.addEventListener('click', fcn);
+
+
+	const search = document.querySelector('.sidebar .search');
+
+	search.addEventListener('input', function(ev) {
+		let regex = new RegExp( ev.target.value, 'i');
+
+		methods.querySelectorAll('a').forEach(function(item) {
+			if(regex.test(item.textContent)) {
+				item.classList.remove('hide');
+			} else {
+				item.classList.add('hide');
+			}
+		});
+	});
+})();
