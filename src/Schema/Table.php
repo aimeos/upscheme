6073dc76a5511231df1016a04031313154419623
Aimeos <aimeos@aimeos.org>
2024-10-20 17:42:23 +0200

    Fixed quoting for local column name in foreign() for Oracle

--- src/Schema/Table.php
+++ src/Schema/Table.php
@@ -788,10 +788,10 @@ class Table
 		unset( $options['name'], $options['autoincrement'], $options['charset'], $options['collation'], $options['check'] );
 
 		if( $this->table->hasColumn( $name ) ) {
-			$this->table->modifyColumn( $name, $options + ['platformOptions' => $custom] );
+			$this->table->modifyColumn( $this->db->qi( $name ), $options + ['platformOptions' => $custom] );
 		} else {
 			$types = $column->getType()->getTypeRegistry();
-			$this->table->addColumn( $name, $types->lookupName( $column->getType() ), $options + ['platformOptions' => $custom] );
+			$this->table->addColumn( $this->db->qi( $name ), $types->lookupName( $column->getType() ), $options + ['platformOptions' => $custom] );
 		}
 	}
 
