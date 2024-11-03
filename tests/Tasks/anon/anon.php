4ced7f36f496033d2efc20360cbdb7cd92b30c19
Aimeos <aimeos@aimeos.org>
2024-11-03 07:29:52 +0100

    Fixed coding style

--- tests/Tasks/anon/anon.php
+++ tests/Tasks/anon/anon.php
@@ -6,10 +6,10 @@ namespace Aimeos\Upscheme\Task;
 use Aimeos\Upscheme\Schema\Table;
 
 
-return new class( $this ) extends Base {
-
+return new class( $this ) extends Base
+{
 	public function up()
 	{
-        echo 'anonymous class';
+		echo 'anonymous class';
 	}
 };
