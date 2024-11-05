7b4b8315d9c52b990891b7a4f88b78bdad63091d
Aimeos <aimeos@aimeos.org>
2024-11-05 13:10:30 +0100

    Added workaround for deleting automatically created Oracle sequences

--- tests/Tasks/delete/Delete.php
+++ tests/Tasks/delete/Delete.php
@@ -14,6 +14,7 @@ class Delete extends Base
 
 		$this->dropView( 'testview' )->delete( 'test2' )
 			->dropTable( 'testref2' )->dropTable( 'test2' )
-			->dropSequence( 'seq_test2' );
+			->dropSequence( 'seq_test2' )
+			->dropSequence( 'test_SEQ' )->dropSequence( 'testref_SEQ' ); // workaround for Oracle
 	}
 }
