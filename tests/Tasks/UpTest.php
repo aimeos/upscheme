22d7c6c2c376d560eda4ad8bf06b7413be94fea1
Aimeos <aimeos@aimeos.org>
2024-11-05 12:12:27 +0100

    Split generator tests into several test methods

--- tests/Tasks/UpTest.php
+++ tests/Tasks/UpTest.php
@@ -9,6 +9,7 @@
 class UpTest extends \PHPUnit\Framework\TestCase
 {
 	private $config;
+	private $dir;
 
 
 	protected function setUp() : void
@@ -19,22 +20,35 @@ class UpTest extends \PHPUnit\Framework\TestCase
 		} );
 
 		$this->config = include dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'config.php';
+		$this->dir = dirname( __DIR__ ) . '/tmp';
 	}
 
 
 	public function testCreate()
 	{
-		$dir = dirname( __DIR__ ) . '/tmp';
-		file_exists( $dir ) ?: mkdir( $dir );
+		file_exists( $this->dir ) ?: mkdir( $this->dir );
 
-		\Aimeos\Upscheme\Up::use( $this->config, dirname( __DIR__ ) . '/Tasks/create' )->up();
-
-		$object = \Aimeos\Upscheme\Up::use( $this->config, $dir )->create();
-		\Aimeos\Upscheme\Up::use( $this->config, $dir )->up();
-
-		\Aimeos\Upscheme\Up::use( $this->config, dirname( __DIR__ ) . '/Tasks/rename' )->up();
-		\Aimeos\Upscheme\Up::use( $this->config, dirname( __DIR__ ) . '/Tasks/delete' )->up();
+		$object = \Aimeos\Upscheme\Up::use( $this->config, dirname( __DIR__ ) . '/Tasks/create' )->up();
 
 		$this->assertInstanceOf( \Aimeos\Upscheme\Up::class, $object );
+	}
+
+
+	public function testExecute()
+	{
+		$object = \Aimeos\Upscheme\Up::use( $this->config, $this->dir )->create();
+		$this->assertInstanceOf( \Aimeos\Upscheme\Up::class, $object );
+
+		\Aimeos\Upscheme\Up::use( $this->config, $this->dir )->up();
+	}
+
+
+	public function testCleanup()
+	{
+		$object = \Aimeos\Upscheme\Up::use( $this->config, dirname( __DIR__ ) . '/Tasks/rename' )->up();
+		$object2 = \Aimeos\Upscheme\Up::use( $this->config, dirname( __DIR__ ) . '/Tasks/delete' )->up();
+
+		$this->assertInstanceOf( \Aimeos\Upscheme\Up::class, $object );
+		$this->assertInstanceOf( \Aimeos\Upscheme\Up::class, $object2 );
 	}
 }
